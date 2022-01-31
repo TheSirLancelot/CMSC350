768dd3176339498e4d166fd6a6339031e6f55edf
TheSirLancelot <williamweir10@gmail.com>
2022-01-31 17:52:35 -0500

    New project

---
+++ Project2/src/project2/Polynomial.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package project2;
+
+import java.util.Iterator;
+
+/**
+ * Filename: Polynomial.java
+ * @author William Weir
+ * Date: Jan 31, 2022
+ * Description: Class that contains all Polynomial functionality
+ */
+public class Polynomial implements 
+	Iterable<Polynomial.Term>, 
+	Comparable<Polynomial> 
+{
+
+	public class Term {
+
+	}
+
+	@Override
+	public int compareTo(Polynomial o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Iterator<Term> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
