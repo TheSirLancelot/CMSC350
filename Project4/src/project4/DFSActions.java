a1ccd8ab3f559fc4da0152f9858d8e30d0689b5c
TheSirLancelot <williamweir10@gmail.com>
2022-03-07 16:02:01 -0500

    First commit

---
+++ Project4/src/project4/DFSActions.java
@@ -0,0 +1,11 @@
+package project4;
+
+public interface DFSActions<V> {
+	public void processVertex(V vertex);
+
+	public void descendVertex(V vertex);
+
+	public void ascendVertex(V vertex);
+
+	public void cycleDetected();
+}
